25c79e82b105a2058ccace63b11852803fc925de
yaya-15 <yajairamaes@gmail.com>
2022-06-23 19:11:26 -0700

    login

--- flutter_application1/lib/main.dart
+++ flutter_application1/lib/main.dart
@@ -24,29 +24,71 @@ class Inicio extends StatefulWidget {
 class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Mi - - App"),
-        ),
-        body: ListView(
-          children: [
-            Container(
-                padding: EdgeInsets.all(10.0),
-                child: Image.network(
-                    "https://scontent.fhmo3-1.fna.fbcdn.net/v/t1.6435-9/110202752_2363782250596048_4063900221828430567_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHpqzyPvx3RQ3KxQsu_35oEiKm_joH_BfSIqb-Ogf8F9LGZ2SUaHWTPvvcERGNFBAml7NB_zWhmN6Jbl9wGSgU0&_nc_ohc=VaG0niC_1IgAX911RHf&_nc_ht=scontent.fhmo3-1.fna&oh=00_AT9r7bBMEN-akL8-RPRsU3Ws5NZMGnoJLYvwHnpyaSAC8A&oe=62D714DB")),
-            Container(
-                padding: EdgeInsets.all(10.0),
-                child: Image.network(
-                    "https://scontent.fhmo3-1.fna.fbcdn.net/v/t1.6435-9/110202752_2363782250596048_4063900221828430567_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHpqzyPvx3RQ3KxQsu_35oEiKm_joH_BfSIqb-Ogf8F9LGZ2SUaHWTPvvcERGNFBAml7NB_zWhmN6Jbl9wGSgU0&_nc_ohc=VaG0niC_1IgAX911RHf&_nc_ht=scontent.fhmo3-1.fna&oh=00_AT9r7bBMEN-akL8-RPRsU3Ws5NZMGnoJLYvwHnpyaSAC8A&oe=62D714DB")),
-            Container(
-                padding: EdgeInsets.all(10.0),
-                child: Image.network(
-                    "https://scontent.fhmo3-1.fna.fbcdn.net/v/t1.6435-9/110202752_2363782250596048_4063900221828430567_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHpqzyPvx3RQ3KxQsu_35oEiKm_joH_BfSIqb-Ogf8F9LGZ2SUaHWTPvvcERGNFBAml7NB_zWhmN6Jbl9wGSgU0&_nc_ohc=VaG0niC_1IgAX911RHf&_nc_ht=scontent.fhmo3-1.fna&oh=00_AT9r7bBMEN-akL8-RPRsU3Ws5NZMGnoJLYvwHnpyaSAC8A&oe=62D714DB")),
-            Container(
-                padding: EdgeInsets.all(10.0),
-                child: Image.network(
-                    "https://scontent.fhmo3-1.fna.fbcdn.net/v/t1.6435-9/110202752_2363782250596048_4063900221828430567_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHpqzyPvx3RQ3KxQsu_35oEiKm_joH_BfSIqb-Ogf8F9LGZ2SUaHWTPvvcERGNFBAml7NB_zWhmN6Jbl9wGSgU0&_nc_ohc=VaG0niC_1IgAX911RHf&_nc_ht=scontent.fhmo3-1.fna&oh=00_AT9r7bBMEN-akL8-RPRsU3Ws5NZMGnoJLYvwHnpyaSAC8A&oe=62D714DB")),
-          ],
-        ));
+    return Scaffold(body: cuerpo());
   }
+}
+
+Widget cuerpo() {
+  return Container(
+    decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage(
+                "https://i.pinimg.com/originals/e0/32/83/e03283a698cdcc4b76f2cc665809a4ce.jpg"),
+            fit: BoxFit.cover)),
+    child: Center(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            nombre(),
+            campoUsuario(),
+            campoContrasena(),
+            //SizedBox(height: 10,), Es una opcion para poner espacio entre los elementos
+            botonEntrar()
+          ]),
+    ),
+  );
+}
+
+Widget nombre() {
+  return Text(
+    "Sing in",
+    style: TextStyle(
+        color: Colors.white, fontSize: 35.0, fontWeight: FontWeight.bold),
+  );
+}
+
+Widget campoUsuario() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+    child: TextField(
+      decoration: InputDecoration(
+          hintText: "User",
+          fillColor: Colors.white, //Declaras que color quieres
+          filled: true //Declaras que SI se pinte de ese color
+          ),
+    ),
+  );
+}
+
+Widget campoContrasena() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Password",
+        fillColor: Colors.white,
+        filled: true,
+      ),
+    ),
+  );
+}
+
+Widget botonEntrar() {
+  return FlatButton(
+    color: Colors.blueGrey,
+    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+    onPressed: () {},
+    child: Text("Enter", style: TextStyle(fontSize: 15, color: Colors.white)),
+  );
 }
